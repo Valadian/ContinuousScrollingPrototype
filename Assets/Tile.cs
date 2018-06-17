625a7f2e129bb5ac7a0769e1b39854621da4d1d3
valadian <berger_jesse@yahoo.com>
2018-06-16 23:50:18 -0500

    Working prototype for continuous scrolling

---
+++ Assets/Tile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Tile : MonoBehaviour {
+    public enum Terrain { Sand, Mountain, Grass, Hills, Water}
+    Terrain terrain;
+    SpriteRenderer sr;
+    public static float X_SIZE = 0.64f;
+    public static float Y_SIZE = 0.20f;
+    public static float ODD_ROW_OFFSET = 0.32f;
+    // Use this for initialization
+    void Start () {
+	}
+    int x = 0;
+    int y = 0;
+    public void Initialize(int x, int y) {
+        this.x = x;
+        this.y = y;
+        transform.position = new Vector3(x * X_SIZE + (y % 2) * ODD_ROW_OFFSET, y * Y_SIZE);
+        sr = GetComponent<SpriteRenderer>();
+        terrain = (Terrain)Random.Range(0, 5);
+        sr.color = GetColor();
+    }
+    Color GetColor() {
+        Color color = Color.white;
+        switch (terrain) {
+            case Terrain.Grass:
+                color = Color.green;
+                break;
+            case Terrain.Hills:
+                color = new Color(173f / 255, 1, 47f / 255);
+                break;
+            case Terrain.Mountain:
+                color = Color.gray;
+                break;
+            case Terrain.Sand:
+                color = Color.yellow;
+                break;
+            case Terrain.Water:
+                color = Color.blue;
+                break;
+        }
+        return color;
+    }
+    public void Illusion(int max_width, int direction) {
+        var shadow = new GameObject("Shadow");
+        shadow.transform.parent = transform;
+        var child_sr = shadow.AddComponent<SpriteRenderer>();
+        child_sr.color = GetColor();
+        child_sr.sprite = sr.sprite;
+        shadow.transform.localPosition = new Vector3(max_width * X_SIZE * direction,0,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TileManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+    public Tile tilePrefab;
+
+    Tile[][] tiles;
+    public int WIDTH = 30;
+    public int HEIGHT = 10;
+    public int PAGE_WIDTH = 12;
+
+    // Use this for initialization
+	void Start () {
+        tiles = Enumerable.Range(0, WIDTH).Select(x => Enumerable.Range(0, HEIGHT).Select(y => {
+            Tile tile = Instantiate(tilePrefab);
+            tile.Initialize(x, y);
+            if(x<= PAGE_WIDTH) {
+                tile.Illusion(WIDTH, 1);
+            }
+            if (x >= WIDTH - PAGE_WIDTH) {
+                tile.Illusion(WIDTH, -1);
+            }
+            return tile;
+        }).ToArray()).ToArray();
+	}
+
+    float SCROLL_SPEED = 10;
+	// Update is called once per frame
+	void Update () {
+        var d = 1-Input.GetAxis("Mouse ScrollWheel");
+        if (d != 1) {
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize * d, 0.25f, 3.2f);
+        }
+        HandleMouseDrag();
+    }
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+    private Vector3 start;
+    void HandleMouseDrag() {
+        if (Input.GetMouseButtonDown(0)) {
+            dragOrigin = Input.mousePosition;
+            start = Camera.main.transform.position;
+            return;
+        }
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed);
+
+        Camera.main.transform.position = start-move*Camera.main.orthographicSize;
+
+        var new_pos = Camera.main.transform.position;
+        var new_x = new_pos.x % (WIDTH * Tile.X_SIZE);
+        if (new_x < 0) {
+            new_x += (WIDTH * Tile.X_SIZE);
+        }
+        Camera.main.transform.position = new Vector3(new_x, Mathf.Clamp(new_pos.y,0,HEIGHT*Tile.Y_SIZE),-10);
+
+
+    }
+}
